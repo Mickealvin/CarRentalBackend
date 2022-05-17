1ffe7c6ab4b261f1a6fd15af862ccae9723463e7
Carlos Diaz <carlos.cedp@gmail.com>
2022-05-17 10:30:31 -0400

    migration applyed

--- CarRental.Infrastructure/Migrations/20220517143003_add_user_model.Designer.cs
+++ CarRental.Infrastructure/Migrations/20220517143003_add_user_model.Designer.cs
@@ -4,14 +4,16 @@ using CarRental.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CarRental.Infrastructure.Migrations
 {
     [DbContext(typeof(CarRentalContext))]
-    partial class CarRentalContextModelSnapshot : ModelSnapshot
+    [Migration("20220517143003_add_user_model")]
+    partial class add_user_model
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
